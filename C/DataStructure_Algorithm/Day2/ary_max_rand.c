5feec43359afaa2947ad507f55385ad49d0dcf9f
hky.u <fab@kakao.com>
2020-01-31 15:25:15 +0900

    Day2: Basic Data Structure(~60p)

---
+++ C/DataStructure_Algorithm/Day2/ary_max_rand.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxof(const int a[], int n) {
+	int i;
+	int max = a[0];
+	for (i = 1; i < n; i++)
+		if (a[i] > max) max = a[i];
+	return max;
+}
+
+int main(void) {
+	int i;
+	int* height;
+	int number;
+	printf("People: ");
+	scanf_s("%d", &number);
+	height = calloc(number, sizeof(int));
+	srand(time(NULL));
+	for (i = 0; i < number; i++) {
+		height[i] = 100 + rand() % 90;
+		printf("height[%d] = %d\n", i, height[i]);
+	}
+	printf("Maximum is %d.\n", maxof(height, number));
+	free(height);
+	return 0;
+}
